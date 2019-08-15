bc81c6fcecf27029795ffe9402cefbf521024e10
GDLMadushanka <gdlmadushanka@gmail.com>
2019-08-15 10:10:42 +0530

    initial commit

---
+++ CustomMessageFormatter/src/main/java/com/wso2/custom/CsvToXmlCustomMessageBuilder.java
@@ -0,0 +1,105 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.wso2.custom;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.util.AXIOMUtil;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.builder.Builder;
+import org.apache.axis2.builder.BuilderUtil;
+import org.apache.axis2.context.MessageContext;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A custom message builder is written to read {@link InputStream} of the axis2 transport and convert CSV data to
+ * plain XML. The purpose is to use in the subsequent transformation to cater to the dynamic behaviors.
+ */
+public class CsvToXmlCustomMessageBuilder implements Builder {
+
+    /**
+     * Read the input stream and build XML payload
+     *
+     * @param inputStream    axis2 transport input stream
+     * @param contentType    message builder content type
+     * @param messageContext axis2 message context
+     * @return process message payload
+     * @throws AxisFault throws if any exception occurred while processing
+     */
+    public OMElement processDocument(InputStream inputStream, String contentType, MessageContext messageContext)
+            throws AxisFault {
+
+        List<String> columns = new ArrayList<>();
+        String charSetEnc = BuilderUtil.getCharSetEncoding(contentType);
+        BufferedReader reader;
+        String line;
+        int lineNumber = 0;
+        OMElement rootElement;
+        try {
+
+            // Create root element
+            rootElement = AXIOMUtil.stringToOM("<root/>");
+            reader = new BufferedReader(new InputStreamReader(inputStream, charSetEnc));
+
+            // Read values from the input stream
+            while ((line = reader.readLine()) != null) {
+
+                lineNumber += 1;
+                String[] rowValues = line.split("\\;", -1);
+
+                // Read columns from the input stream and populate list of columns names
+                if (lineNumber == 1) {
+                    columns.addAll(Arrays.asList(rowValues));
+                    continue;
+                }
+
+                // Create a raw element
+                OMElement rawElement = AXIOMUtil.stringToOM("<raw/>");
+                rootElement.addChild(rawElement);
+
+                // Read row array which represent the raw
+                for (int i = 0; i < columns.size(); i++) {
+                    String column = columns.get(i);
+                    String value;
+
+                    if (i < rowValues.length) {
+                        value = rowValues[i];
+                    } else {
+                        //TODO define suitable default value for missing values
+                        value = "";
+                    }
+
+                    // Create element by column name and inject raw value
+                    OMElement columnElement = AXIOMUtil.stringToOM("<" + column + "/>");
+                    columnElement.setText(value);
+                    rawElement.addChild(columnElement);
+                }
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            throw new AxisFault("Unsupported encoding: " + charSetEnc, e);
+        } catch (IOException | XMLStreamException e) {
+            throw new AxisFault("Error occurred while building content: ", e);
+        }
+        return rootElement;
+    }
+}
